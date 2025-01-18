b02beac2ae80027c34a7c4d8f81be9ba7b9d56ad
V.Prasanth <prasanth.v2023@vitstudent.ac.in>
2025-01-18 12:24:22 +0530

    commit

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,8 +1,8 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env. MONGO_DB_URI);
+		await mongoose.connect(process.env.MONGO_DB_URI);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
